7a9936ff76f08b3e3f5554b768fc807daa77bbc9
yuchuanliu <yuchuanliu@tencent.com>
2019-10-27 23:45:25 +0800

    Shaders

---
+++ ShaderUtils.h
@@ -0,0 +1,70 @@
+//
+// Created by 97269 on 2019/10/27.
+//
+
+#ifndef OPENGL_LEARN_SHADERUTILS_H
+#define OPENGL_LEARN_SHADERUTILS_H
+
+#include <GLFW/glfw3.h>
+#include <glad/glad.h>
+#include <iostream>
+
+int compileSourceShader(int type, const char *source) {
+    int shaderId = glCreateShader(type);
+    if (shaderId == 0) {
+        return 0;
+    }
+
+    glShaderSource(shaderId, 1, &source, nullptr);
+    glCompileShader(shaderId);
+
+    int success;
+    glGetShaderiv(shaderId, GL_COMPILE_STATUS, &success);
+
+    if (!success) {
+        char infoLog[512];
+        glGetShaderInfoLog(shaderId, 512, nullptr, infoLog);
+        std::cerr << "Compile Shader failed! Info=" << infoLog << std::endl;
+        glDeleteShader(shaderId);
+        return 0;
+    }
+
+    return shaderId;
+}
+
+int createGLProgram(const char *vertexShaderSource, const char *fragShaderSource) {
+    int vertexShaderId = compileSourceShader(GL_VERTEX_SHADER, vertexShaderSource);
+    if (vertexShaderId == 0) {
+        return 0;
+    }
+    int fragShaderId = compileSourceShader(GL_FRAGMENT_SHADER, fragShaderSource);
+    if (fragShaderId == 0) {
+        return 0;
+    }
+    int programId = glCreateProgram();
+
+    if (programId == 0) {
+        return 0;
+    }
+
+    glAttachShader(programId, vertexShaderId);
+    glAttachShader(programId, fragShaderId);
+    glLinkProgram(programId);
+    int success;
+    glGetProgramiv(programId, GL_LINK_STATUS, &success);
+    if (!success) {
+        char infoLog[512];
+        glGetProgramInfoLog(programId, 512, nullptr, infoLog);
+        std::cerr << "Cannot link program! InfoLog=" << infoLog << std::endl;
+        glDeleteProgram(programId);
+        glDeleteShader(vertexShaderId);
+        glDeleteShader(fragShaderId);
+        return 0;
+    }
+
+    glDeleteShader(vertexShaderId);
+    glDeleteShader(fragShaderId);
+    return programId;
+}
+
+#endif //OPENGL_LEARN_SHADERUTILS_H
--- main.cpp
+++ main.cpp
@@ -1,8 +1,16 @@
 #include <iostream>
 #include <glad/glad.h>
 #include <GLFW/glfw3.h>
+#include "Shaders.h"
+#include "ShaderUtils.h"
 
 void framebuffer_size_callback(GLFWwindow *window, int width, int height);
+
+void processInput(GLFWwindow *window);
+
+void drawFrame();
+
+void initProgram();
 
 int main() {
     glfwInit();
@@ -26,7 +34,14 @@ int main() {
 
     glfwSetFramebufferSizeCallback(window, framebuffer_size_callback);
 
+    initProgram();
+
     while (!glfwWindowShouldClose(window)) {
+        processInput(window);
+
+        glClearColor(0.2f, 0.3f, 0.3f, 1.0f);
+        glClear(GL_COLOR_BUFFER_BIT);
+
         glfwPollEvents();
         glfwSwapBuffers(window);
     }
@@ -37,4 +52,34 @@ int main() {
 
 void framebuffer_size_callback(GLFWwindow *window, int width, int height) {
     glViewport(0, 0, width, height);
+}
+
+void processInput(GLFWwindow *window) {
+    if (glfwGetKey(window, GLFW_KEY_ESCAPE) == GLFW_PRESS) {
+        glfwSetWindowShouldClose(window, true);
+    }
+}
+
+void drawFrame() {
+
+}
+
+void initProgram() {
+    std::string vertexProgram = SIMPLE_VERTEX_SHADER;
+    std::string fragProgram = SIMPLE_FRAG_SHADER;
+    int programId = createGLProgram(vertexProgram.c_str(), fragProgram.c_str());
+    std::cout << "ProgramId=" << programId << std::endl;
+    glUseProgram(programId);
+
+    float vertices[] = {
+            -0.5f, -0.5f, 0.0f,
+            0.5f, -0.5f, 0.0f,
+            0.0f, 0.5f, 0.0f
+    };
+    unsigned int VBO;
+    glGenBuffers(1, &VBO);
+    glBindBuffer(GL_ARRAY_BUFFER, VBO);
+    glBufferData(GL_ARRAY_BUFFER, sizeof(vertices), vertices, GL_STATIC_DRAW);
+    glVertexAttribPointer(0, 3, GL_FLOAT, GL_FALSE, 3 * sizeof(float), nullptr);
+    glEnableVertexAttribArray(0);
 }
